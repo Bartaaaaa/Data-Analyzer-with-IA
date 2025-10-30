7ef182582c94282cfc2e0b57fd50e372ae8b1233
Bartlomiej Grzadziel <bartlomiejgrzadziel2@gmail.com>
2025-10-30 14:23:37 +0100

    better spotifyDada urls

--- backend/src/urls/SpotifyUserDataUrl.py
+++ backend/src/urls/SpotifyUserDataUrl.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from src.views.SpotifyUserDataView import (
-    SpotifyTopArtists
+    SpotifyTopArtists,
+    SpotifyTopTracks
 )
 
 urlpatterns = [
     path("topArtists", SpotifyTopArtists.as_view(), name="spotify-topArtists"),
+    path("topTracks", SpotifyTopTracks.as_view(), name="spotify-topTracks"),
 ]
--- backend/src/views/SpotifyUserDataView.py
+++ backend/src/views/SpotifyUserDataView.py
@@ -14,7 +14,9 @@ class SpotifyTopArtists(APIView):
         token_helper = SpotifyTokenMixin()
         access_token = token_helper.getValidSpotifyToken(user)
         headers = {"Authorization": f"Bearer {access_token}"}
-        params = {"limit" : 10, "time_range": "medium_term"}
+        limit = int(request.GET.get("limit", 10))
+        time_range = request.GET.get("time_range", "medium_term")
+        params = {"limit" :limit, "time_range": time_range}
         url = "https://api.spotify.com/v1/me/top/artists"
         spotifyTopArtistRequest = requests.get(url, headers=headers, params=params, verify=False)
         data = spotifyTopArtistRequest.json()
@@ -32,3 +34,35 @@ class SpotifyTopArtists(APIView):
 
         return JsonResponse({"top_artists": top_artists})
 
+@extend_schema(tags=['SpotifyUserData'])
+class SpotifyTopTracks(APIView):
+    def get(self, request):
+        user = request.user
+        token_helper = SpotifyTokenMixin()
+        access_token = token_helper.getValidSpotifyToken(user)
+        headers = {"Authorization": f"Bearer {access_token}"}
+        limit = int(request.GET.get("limit", 10))
+        time_range = request.GET.get("time_range", "medium_term")
+        offset = int(request.GET.get("offset", 0))
+        params = {"limit" :limit, "time_range": time_range, "offset": offset}
+        url = "https://api.spotify.com/v1/me/top/tracks"
+        spotifyTopTracksRequest = requests.get(url, headers=headers, params=params, verify=False)
+        data = spotifyTopTracksRequest.json()
+        top_tracks = [
+            {
+                "name": track["name"],
+                "id": track["id"],
+                "artists": [artist["name"] for artist in track["artists"]],
+                "popularity": track["popularity"],
+                "image": track["album"]["images"][0]["url"] if track["album"]["images"] else None,
+                "album": track["album"]["name"],
+                "album_type" : track["album"]["type"],
+                "album_id" : track["album"]["id"],
+                "album_url" : track["album"]["external_urls"]["spotify"],
+                "spotify_url": track["external_urls"]["spotify"],
+                "preview_url": track.get("preview_url"),
+            }
+            for track in data.get("items", [])
+        ]
+
+        return JsonResponse({"top_tracks": top_tracks})
